a18b082548cdb386ff9c11dbb730359259abd22d
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2013-07-01 15:08:17 -0400

    Merge MessageReceiving and -ExchangingOperations

--- src/main/java/messaging/MessageDestinationReceivingOperations.java
+++ src/main/java/messaging/MessageDestinationReceivingOperations.java
@@ -2,11 +2,18 @@ package messaging;
 
 import org.springframework.integration.Message;
 import org.springframework.integration.MessagingException;
+import org.springframework.integration.core.MessagePostProcessor;
 
 public interface MessageDestinationReceivingOperations<D> extends MessageReceivingOperations {
 
 	<P> Message<P> receive(D destination) throws MessagingException;
 
 	Object receiveAndConvert(D destination) throws MessagingException;
+
+	Message<?> sendAndReceive(D destination, Message<?> requestMessage);
+
+	Object convertSendAndReceive(D destination, Object request);
+
+	Object convertSendAndReceive(D destination, Object request, MessagePostProcessor requestPostProcessor);
 
 }
--- src/main/java/messaging/MessageReceivingOperations.java
+++ src/main/java/messaging/MessageReceivingOperations.java
@@ -2,6 +2,7 @@ package messaging;
 
 import org.springframework.integration.Message;
 import org.springframework.integration.MessagingException;
+import org.springframework.integration.core.MessagePostProcessor;
 
 public interface MessageReceivingOperations {
 
@@ -12,5 +13,17 @@ public interface MessageReceivingOperations {
 	Object receiveAndConvert() throws MessagingException;
 
 	Object receiveAndConvert(String destinationName) throws MessagingException;
+
+	Message<?> sendAndReceive(Message<?> requestMessage);
+
+	Message<?> sendAndReceive(String destinationName, Message<?> requestMessage);
+
+	Object convertSendAndReceive(Object request);
+
+	Object convertSendAndReceive(String destinationName, Object request);
+
+	Object convertSendAndReceive(Object request, MessagePostProcessor requestPostProcessor);
+
+	Object convertSendAndReceive(String destinationName, Object request, MessagePostProcessor requestPostProcessor);
 
 }
